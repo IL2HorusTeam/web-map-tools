83ea5b11359c6f2a52222086d4c9e1c1b4f67998
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-03-16 13:59:43 +0200

    url for static files modified

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.base import RedirectView
 
 admin.autodiscover()
@@ -14,7 +15,5 @@ urlpatterns = patterns('',
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
 )
 
-if settings.DEBUG is False:   #if DEBUG is True it will be served automatically
-    urlpatterns += patterns('',
-        (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+if not settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
