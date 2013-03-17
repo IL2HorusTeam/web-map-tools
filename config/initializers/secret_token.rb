e63632b6754ad6543d54823627e57d8006e795c0
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-03-17 11:36:03 +0200

    secret_token initialization fixed

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MapTools::Application.config.secret_token = 'f70a5e30d5812a8e3446518d1bab747702326d52c0ff4fe20c12042bd57e361a578e97bf986c84c7e8996a6cc00c1d6bb66fadb2ea640d8a014dca49e3544d61'
+MapTools::Application.config.secret_token = \
+    ENV['HORUS_WEB_MAPTOOLS_SECRET_TOKEN'] || \
+    'f70a5e30d5812a8e3446518d1bab747702326d52c0ff4fe20c12042bd57e361a578e97bf986c84c7e8996a6cc00c1d6bb66fadb2ea640d8a014dca49e3544d61'
