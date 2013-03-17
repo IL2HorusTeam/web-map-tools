dc93d988763e7d35dfa93992d88c58ceaf776b30
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-03-17 12:56:09 +0200

    assets precompilation fixed

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module MapTools
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Added to fix devise/active admin issue
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
