63f7d499169d91f8e2ba01c50eab685db68d809c
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-03-17 12:50:20 +0200

    assets precompilation reverted

--- config/application.rb
+++ config/application.rb
@@ -56,9 +56,6 @@ module MapTools
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    # Needed by Heroku
-    config.assets.initialize_on_precompile = false
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
